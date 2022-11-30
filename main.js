07dc67cb203310a3d51e456e439ffcb77963869a
Francesca-Barcella <pollon.fra@gmail>
2022-11-30 17:20:33 +0100

    aggiunta chiamata axios

--- main.js
+++ main.js
@@ -5,7 +5,61 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            tasks: [],
+            api_url: 'server.php'
         }
-    }
+    },
+    methods:{
+        callApi(url){
+            axios.get(url)
+            .then(response => {
+                console.log(response.data);
+                this.tasks = response.data;
+            })
+            //questo metodo .catch serve per intercettera eventuali errori
+            .catch(err => {
+                console.error(err.message);
+
+            })
+        }
+    },
+    mounted() {
+        this.callApi(this.api_url)
+    
+    },
 }).mount('#app')
+
+// ci sarà url che corrispende al nostro server.php
+// in return array vuoto tasks[]
+// struttura della chiamata nuova presa dalle slide:esempio chiamata axios da esercizio vecchio
+
+/* methods: {
+    callApi(url) {
+        console.log(this.state.filterCategory + 'prova riga 22');
+        if (this.state.filterCategory !== '') {
+            url += `?${this.state.categoryParameter}=${this.state.filterCategory}`
+        }
+        console.log(url);
+
+        axios.get(url)
+            .then(response => {
+                //console.log(response.data);
+                this.state.characters = response.data
+                this.state.loading = false
+
+            })
+            //questo metodo .catch serve per intercettera eventuali errori
+            .catch(err => {
+                console.error(err.message);
+                this.state.error = err.message
+                this.state.loading = false
+            })
+    }
+},
+mounted() {
+    this.callApi(this.state.API_URL)
+
+}, */
+
+
+
--- server.php
+++ server.php
@@ -9,7 +9,7 @@ $tasks = [
     'Laravel'
 ];
 
-var_dump($tasks);
+//var_dump($tasks);
 //aggiungo funzione header() + json_encode per far leggere a js la lista dei tasks
 header ('Content-Type:application/json');
 echo json_encode($tasks);
