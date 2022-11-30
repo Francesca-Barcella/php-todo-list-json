33e4892ebe3da8897c2279c9190115081f3262d7
Francesca-Barcella <pollon.fra@gmail>
2022-11-30 17:41:37 +0100

    sistemato ordine degli import +  stamp di tasks

--- index.php
+++ index.php
@@ -52,11 +52,11 @@ Abilitare l’eliminazione di un task */
     </div>
 
 
-    <!-- link per js -->
-    <script src="./main.js"></script>
-    <!-- link per vue -->
-    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
     <!-- link per axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js" integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- link per vue -->
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <!-- link per js -->
+    <script src="./main.js"></script>
 </body>
 </html>
