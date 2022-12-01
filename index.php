f81cb9c9ad15f790e4919af579c111f5d1809d4f
Francesca-Barcella <pollon.fra@gmail>
2022-12-01 08:25:50 +0100

    aggiunto form per aggiunta tasks

--- index.php
+++ index.php
@@ -23,22 +23,21 @@ Abilitare l’eliminazione di un task */
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
+
 <body>
     <div id="app">
-        <div class="container" >
+        <div class="container">
 
             <h1>To do list</h1>
 
@@ -46,8 +45,17 @@ Abilitare l’eliminazione di un task */
             <ul>
                 <li v-for="task in tasks">{{task}}</li>
             </ul>
-            
-            
+
+            <!-- aggiungere nuovi tasks -> form con action server.php + method post (per inviare dati al server) -->
+            <form action="server.php" method="post">
+                <div class="mb-3">
+                    <label for="new_task" class="form-label">Inserisci nuovo task</label>
+                    <input type="text" name="new_task" id="new_task" class="form-control" placeholder="nuovo task">
+                </div>
+                <button type="submit" class="p-1 me-1">Aggiungi</button>
+                <button type="reset" class="p-1">Annulla</button>
+            </form>
+
         </div>
     </div>
 
@@ -59,4 +67,5 @@ Abilitare l’eliminazione di un task */
     <!-- link per js -->
     <script src="./main.js"></script>
 </body>
+
 </html>
